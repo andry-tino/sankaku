1c819e3289ba00af805e4ba2106bff2eb4ef702e
Andrea <andry.tino@gmail.com>
2015-09-10 19:09:17 +0200

    Fixed wrong import + added test for `sourceLoader`.

--- test/sourceLoader-testSuite/sourceLoader-ut.js
+++ test/sourceLoader-testSuite/sourceLoader-ut.js
@@ -25,15 +25,29 @@ module.exports = {
   /**
    * Gets the source successfully.
    */
-  getSource: function(test) {
+  getSourceFromValidFile: function(test) {
     test.expect(4);
 
     var src = sourceLoader('test-source1.md');
     
-    test.ok(!src.error, 'No error should occur in loading file!');
-    test.ok(src.source, 'Source should have been loaded!');
+    test.ok(src.error === null, 'No error should occur in loading file!');
+    test.ok(src.source !== null, 'Source should have been loaded!');
     test.ok(typeof src === 'string', 'Loaded source should be proper string!');
     test.ok(src.length > 0, 'Loaded source should contain at least 1 character!');
+  
+    test.done();
+  },
+  
+  /**
+   * Gets the source not successfully.
+   */
+  getSourceFromInvalidFile: function(test) {
+    test.expect(2);
+
+    var src = sourceLoader('invalid-file.inv');
+    
+    test.ok(src.error !== null, 'Error should have been swallowed when loading file!');
+    test.ok(src.source === null, 'Source should not have been loaded!');
   
     test.done();
   }
